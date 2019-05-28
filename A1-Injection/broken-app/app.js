748abdcb1ff56c23b9a77ce0d99452e980d5e47c
Justin Dennison <justin1dennison@gmail.com>
2019-05-28 10:30:54 -0400

    added styles for ecommerce application

--- A1-Injection/broken-app/app.js
+++ A1-Injection/broken-app/app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 
+//Templating
 const nunjucks = require('nunjucks')
-
 nunjucks.configure('views', {
   autoescape: true,
   express: app
 })
+//Middleware
+app.use(express.static('./static'))
 
 app.get('/', (req, res) => {
   res.render('home.nj')

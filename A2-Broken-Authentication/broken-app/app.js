9f273815690ee753b45489664c3a4fdacefd5de5
Justin Dennison <justin1dennison@gmail.com>
2019-06-03 13:13:20 -0400

    setup of the views and basic styling

--- A2-Broken-Authentication/broken-app/app.js
+++ A2-Broken-Authentication/broken-app/app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 
+const nunjucks = require('nunjucks')
+nunjucks.configure('views', {
+  express: app
+})
 
-
+app.get('/', (req, res) => {
+  res.render('index.nj')
+})
 
 module.exports = app
